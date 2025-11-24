10dc5fe857ac9a8ed967f14c75b0fb3afdd1a64e
Strabast <strabast@my.yorku.ca>
2025-11-24 18:44:57 -0500

    Tutorial Page Additions

--- upperdut/pause_menu.gd
+++ upperdut/pause_menu.gd
@@ -5,14 +5,30 @@ extends Node2D
 @onready var tutorial: TextureButton = $PausePlaceholder/Tutorial
 @onready var pause: TextureButton = $Pause
 @onready var pause_placeholder: Sprite2D = $PausePlaceholder
+@onready var tutorialpic: Sprite2D = $PausePlaceholder/TutorialPic
+@onready var tutorialback: TextureButton = $PausePlaceholder/TutorialBack
 
 
 func _process(delta: float) -> void:
 	if play.button_pressed:
 		pause_placeholder.visible = false
 		pause.visible = true
+		tutorialpic.visible = false
+		tutorialback.visible = false
+	
 	if quit.button_pressed:
 		get_tree().quit()
+	
 	if pause.button_pressed:
 		pause_placeholder.visible = true
 		pause.visible = false
+		tutorialpic.visible = false
+		tutorialback.visible = false
+	
+	if tutorial.button_pressed:
+		tutorialpic.visible = true
+		tutorialback.visible = true
+	
+	if tutorialback.button_pressed:
+		tutorialpic.visible = false
+		tutorialback.visible = false
